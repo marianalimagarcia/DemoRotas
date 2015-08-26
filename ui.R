4d363478db89f73d7cd26fa43c6a5a43a9c198aa
Mariana Lima Garcia <marianalimagarcia@gmail.com>
2015-08-26 14:11:57 -0300

    modi54

--- ui.R
+++ ui.R
@@ -9,27 +9,27 @@ shinyUI(fluidPage(
     tags$link(rel="stylesheet", type="text/css", href="custom_styles.css")
   ),
   
-  title = "Traveling Salesman with Simulated Annealing, Shiny, and R",
+  title = "Cálculo de Rotas com baixo Custo Computacional",
   
-  tags$h2(tags$a(href="/traveling-salesman", "Traveling Salesman", target="_blank")),
+  tags$h2(tags$a(href="/traveling-salesman", "Cálculo de Rotas", target="_blank")),
   
   plotOutput("map", height="550px"),
   
   fluidRow(
     column(5,
       tags$ol(
-        tags$li("Customize the list of cities, based on the world or US map"),
-        tags$li("Adjust simulated annealing parameters to taste"),
-        tags$li("Click the 'solve' button!")
+        tags$li("Personalize a lista de cidades , baseado no mapa mundial ou no mapa do Brasil"),
+        tags$li("Ajuste os parâmetros do método de arrefecimento ao seu gosto"),
+        tags$li("Click no botão Resolver")
       )
     ),
     column(3,
-      tags$button("SOLVE", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
+      tags$button("Resolver", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
     ),
     column(3,
       HTML("<button id='set_random_cities_2' class='btn btn-large action-button shiny-bound-input'>
               <i class='fa fa-refresh'></i>
-              Set Cities Randomly
+              Definir cidades aleatoriamente
             </button>")
     ), class="aaa"
   ),
@@ -38,23 +38,23 @@ shinyUI(fluidPage(
   
   fluidRow(
     column(5,
-      h4("Choose a map and which cities to tour"),
-      selectInput("map_name", NA, c("World", "USA","Brazil"), "World", width="100px"),
-      p("Type below to select individual cities, or", actionButton("set_random_cities", "set randomly", icon=icon("refresh"))),
+      h4("Selecione um mapa"),
+      selectInput("map_name", NA, c("World", "Brazil"), "World", width="100px"),
+      p("Escreva abaixo o nome das cidades, ou", actionButton("set_random_cities", "Definir Aleatoriamente", icon=icon("refresh"))),
       selectizeInput("cities", NA, all_cities$full.name, multiple=TRUE, width="100%",
                      options = list(maxItems=30, maxOptions=100, placeholder="Start typing to select some cities...",
                                     selectOnTab=TRUE, openOnFocus=FALSE, hideSelected=TRUE)),
-      checkboxInput("label_cities", "Label cities on map?", FALSE)
+      checkboxInput("label_cities", "Colocar rótulos das cidades no mapa?", FALSE)
     ),
     
     column(2,
-      h4("Simulated Annealing Parameters"),
+      h4("Parâmetros do Arrefecimento"),
       inputPanel(
-        numericInput("s_curve_amplitude", "S-curve Amplitude", 4000, min=0, max=10000000),
-        numericInput("s_curve_center", "S-curve Center", 0, min=-1000000, max=1000000),
-        numericInput("s_curve_width", "S-curve Width", 3000, min=1, max=1000000),
-        numericInput("total_iterations", "Number of Iterations to Run", 25000, min=0, max=1000000),
-        numericInput("plot_every_iterations", "Draw Map Every N Iterations", 1000, min=1, max=1000000)
+        numericInput("s_curve_amplitude", "Amplitude da curva S", 4000, min=0, max=10000000),
+        numericInput("s_curve_center", "Centro da curva S", 0, min=-1000000, max=1000000),
+        numericInput("s_curve_width", "Largura da curva S", 3000, min=1, max=1000000),
+        numericInput("total_iterations", "Número de Iterações da Execução", 25000, min=0, max=1000000),
+        numericInput("plot_every_iterations", "Desenhar no mapa a cada n iterações", 1000, min=1, max=1000000)
       ),
       class="numeric-inputs"
     ),
