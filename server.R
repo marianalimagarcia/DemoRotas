211e5c7b9dc1eae7df2fa4d4cc7ed943f33d7147
Mariana Lima Garcia <marianalimagarcia@gmail.com>
2015-08-31 17:32:31 -0300

    3108

--- server.R
+++ server.R
@@ -165,8 +165,8 @@ shinyServer(function(input, output, session) {
     plot_tour(vals$cities, vals$tour, vals$great_circles, map_name, label_cities=input$label_cities)
     
     if (length(vals$tour) > 1) {
-      pretty_dist = prettyNum(vals$tour_distance, big.mark=",", digits=0, scientific=FALSE)
-      pretty_iter = prettyNum(vals$iter, big.mark=",", digits=0, scientific=FALSE)
+      pretty_dist = prettyNum(vals$tour_distance, big.mark=".", digits=0, scientific=FALSE)
+      pretty_iter = prettyNum(vals$iter, big.mark=".", digits=0, scientific=FALSE)
       pretty_temp = prettyNum(current_temperature(vals$iter, vals$s_curve_amplitude, vals$s_curve_center, vals$s_curve_width),
                               big.mark=".", digits=0, scientific=FALSE)
       
